04ac229a21612284e20d411ac82cfc0225bb9541
Krywion <krywionek.maks@gmail.com>
2024-10-27 11:12:49 +0100

    code refactoring

--- src/main/java/pl/krywion/usosremastered/config/security/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package pl.krywion.usosremastered.config.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-public class SecurityConfig {
-
-
-}
