d14bdb299c6b41f75de3af056fc6932d79553308
Krywion <krywionek.maks@gmail.com>
2024-11-13 13:49:12 +0100

    refactor(user): split user service into specialized interfaces
    - Add UserCreator interface for student/employee user creation
    - Add UserManager interface for user management operations
    - Add UserQuery interface for user queries
    - Add implementations for each interface

---
+++ src/main/java/pl/krywion/usosremastered/service/user/api/UserManager.java
@@ -0,0 +1,9 @@
+package pl.krywion.usosremastered.service.user.api;
+
+
+import pl.krywion.usosremastered.entity.User;
+
+public interface UserManager {
+    void deleteUser(String email);
+    User updateUserEmail(String oldEmail, String newEmail);
+}
