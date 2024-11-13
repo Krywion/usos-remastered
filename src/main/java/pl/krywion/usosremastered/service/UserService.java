d258ca678022ae13daafc141eeeff6744e4f25ca
Krywion <krywionek.maks@gmail.com>
2024-11-13 13:49:34 +0100

    refactor(user): update UserService to use specialized interfaces
    - Update UserService interface
    - Implement UserServiceImpl using specialized interfaces
    - Remove direct repository dependencies
    - Delegate operations to proper interfaces

--- src/main/java/pl/krywion/usosremastered/service/UserService.java
+++ src/main/java/pl/krywion/usosremastered/service/UserService.java
@@ -1,17 +1,17 @@
 package pl.krywion.usosremastered.service;
 
-import pl.krywion.usosremastered.config.security.Role;
+import org.springframework.stereotype.Service;
 import pl.krywion.usosremastered.entity.User;
 
 import java.util.List;
 
+@Service
 public interface UserService {
+    User createUserForStudent(String email);
+    User createUserForEmployee(String email);
+    void deleteUser(String email);
+    User updateUserEmail(String oldEmail, String newEmail);
+    User findByEmail(String email);
+    List<User> getAllUsers();
 
-    User createUser(String email, Role role);
-
-    List<User> allUsers();
-
-    User updateUser(String email, Role role);
-
-    User deleteUser(String email);
 }
---
+++ src/main/java/pl/krywion/usosremastered/service/user/impl/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package pl.krywion.usosremastered.service.user.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import pl.krywion.usosremastered.entity.User;
+import pl.krywion.usosremastered.service.UserService;
+import pl.krywion.usosremastered.service.user.api.UserCreator;
+import pl.krywion.usosremastered.service.user.api.UserManager;
+import pl.krywion.usosremastered.service.user.api.UserQuery;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserQuery userQuery;
+    private final UserCreator userCreator;
+    private final UserManager userManager;
+
+    @Override
+    public User createUserForStudent(String email) {
+        return userCreator.createUserForStudent(email);
+    }
+
+    @Override
+    public User createUserForEmployee(String email) {
+        return userCreator.createUserForEmployee(email);
+    }
+
+    @Override
+    public void deleteUser(String email) {
+        userManager.deleteUser(email);
+    }
+
+    @Override
+    public User updateUserEmail(String oldEmail, String newEmail) {
+        return userManager.updateUserEmail(oldEmail, newEmail);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userQuery.findByEmail(email);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userQuery.getAllUsers();
+    }
+}
