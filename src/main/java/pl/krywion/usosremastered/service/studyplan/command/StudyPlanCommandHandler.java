8752b4f300e0b67a087cbbe33e22cdff141b4b78
Krywion <krywionek.maks@gmail.com>
2024-11-16 20:15:55 +0100

    refactor: move ServiceResponse wrapping into controller class
    - Update StudyPlanService
    - Update StudyPlanController
    - Update StudentCommandHandler

--- src/main/java/pl/krywion/usosremastered/service/studyplan/command/StudyPlanCommandHandler.java
+++ src/main/java/pl/krywion/usosremastered/service/studyplan/command/StudyPlanCommandHandler.java
@@ -1,12 +1,9 @@
 package pl.krywion.usosremastered.service.studyplan.command;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.krywion.usosremastered.dto.domain.StudyPlanDto;
 import pl.krywion.usosremastered.dto.domain.mapper.StudyPlanMapper;
-import pl.krywion.usosremastered.dto.response.ServiceResponse;
 import pl.krywion.usosremastered.entity.StudyPlan;
 import pl.krywion.usosremastered.repository.StudyPlanRepository;
 
@@ -19,13 +16,9 @@ public class StudyPlanCommandHandler {
     private final StudyPlanMapper mapper;
 
 
-    public ServiceResponse<StudyPlanDto> handle(CreateStudyPlanCommand createStudyPlanCommand) {
-        StudyPlan studyPlan = mapper.toEntity(createStudyPlanCommand.studyPlanDto());
+    public StudyPlan handle(CreateStudyPlanCommand command) {
+        StudyPlan studyPlan = mapper.toEntity(command.studyPlanDto());
         studyPlanRepository.save(studyPlan);
-        return ServiceResponse.success(
-                mapper.toDto(studyPlan),
-                "Study plan created successfully",
-                HttpStatus.CREATED
-        );
+        return studyPlan;
     }
 }
--- src/main/java/pl/krywion/usosremastered/service/studyplan/query/StudyPlanQueryService.java
+++ src/main/java/pl/krywion/usosremastered/service/studyplan/query/StudyPlanQueryService.java
@@ -1,14 +1,13 @@
 package pl.krywion.usosremastered.service.studyplan.query;
 
 import org.springframework.stereotype.Service;
-import pl.krywion.usosremastered.dto.domain.StudyPlanDto;
-import pl.krywion.usosremastered.dto.response.ServiceResponse;
+import pl.krywion.usosremastered.entity.StudyPlan;
 
 import java.util.List;
 
 @Service
 public interface StudyPlanQueryService {
 
-    ServiceResponse<List<StudyPlanDto>> findAll();
-    ServiceResponse<StudyPlanDto> findById(Long id);
+    List<StudyPlan> findAll();
+    StudyPlan findById(Long id);
 }
