c011ca25a3c74f0b1f60cc4c3c68931c70e568c3
Krywion <krywionek.maks@gmail.com>
2024-11-21 14:42:07 +0100

    fix: fix generateSecurePassword method

--- src/main/java/pl/krywion/usosremastered/service/impl/PasswordManagementServiceImpl.java
+++ src/main/java/pl/krywion/usosremastered/service/impl/PasswordManagementServiceImpl.java
@@ -4,10 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.krywion.usosremastered.service.PasswordManagementService;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 @Service
 public class PasswordManagementServiceImpl implements PasswordManagementService {
@@ -20,33 +17,37 @@ public class PasswordManagementServiceImpl implements PasswordManagementService 
 
     @Override
     public String generateSecurePassword() {
-        int length = 12;
         String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-        String digits = "0123456789";
+        String numbers = "0123456789";
         String specialCharacters = "!@#$%^&*()-+";
-        String allCharacters = upperCaseLetters + lowerCaseLetters + digits + specialCharacters;
 
         StringBuilder password = new StringBuilder();
         Random random = new Random();
 
+        // Add one character from each set
         password.append(upperCaseLetters.charAt(random.nextInt(upperCaseLetters.length())));
         password.append(lowerCaseLetters.charAt(random.nextInt(lowerCaseLetters.length())));
-        password.append(digits.charAt(random.nextInt(digits.length())));
+        password.append(numbers.charAt(random.nextInt(numbers.length())));
         password.append(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
 
-        for (int i = 4; i < length; i++) {
+        String allCharacters = upperCaseLetters + lowerCaseLetters + numbers + specialCharacters;
+
+        // Add random characters from all sets
+        for (int i = 4; i < 12; i++) {
             password.append(allCharacters.charAt(random.nextInt(allCharacters.length())));
         }
 
-        List<Character> passwordChars = password.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.toList());
-        Collections.shuffle(passwordChars);
+        // Shuffle the password
+        char[] passwordArray = password.toString().toCharArray();
+        for (int i = passwordArray.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char temp = passwordArray[i];
+            passwordArray[i] = passwordArray[j];
+            passwordArray[j] = temp;
+        }
 
-        return passwordChars.stream()
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                .toString();
+        return new String(passwordArray);
     }
 
     @Override
