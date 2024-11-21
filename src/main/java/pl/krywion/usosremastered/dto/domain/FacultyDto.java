a175d082fb93d5c976a183b12730152707248000
Krywion <krywionek.maks@gmail.com>
2024-11-21 22:49:09 +0100

    refactor: remove unused lombok property

--- src/main/java/pl/krywion/usosremastered/dto/domain/FacultyDto.java
+++ src/main/java/pl/krywion/usosremastered/dto/domain/FacultyDto.java
@@ -1,8 +1,10 @@
 package pl.krywion.usosremastered.dto.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 public class FacultyDto {
     private Long id;
     private String name;
--- src/main/java/pl/krywion/usosremastered/dto/domain/InstituteDto.java
+++ src/main/java/pl/krywion/usosremastered/dto/domain/InstituteDto.java
@@ -1,8 +1,10 @@
 package pl.krywion.usosremastered.dto.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
 public class InstituteDto {
     private Long id;
     private String name;
